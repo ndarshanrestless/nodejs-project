9fa51cc5ffd87d520b1cbf07f9d62a13fd5307e2
Darshan Nagarajappa <darshan.nagarajappa@autodesk.com>
2017-06-13 11:11:50 -0700

    some temp dont know

--- node-js-playlist-master/express.js
+++ node-js-playlist-master/express.js
@@ -2,14 +2,28 @@
 var express = require('express');
 
 var app = express();
+var bodyParser = require('body-parser')
+var urlencodedParser = bodyParser.urlencoded({ extended: false })
+
 
 app.set('view engine', 'ejs');
 
-app.get('/profile/:name', function(req, res){
-  //res.send('This is the homepage damit! ')
-  var data = {name:req.params.name , age:24,
-    hobbies:['eating', 'sleeping', 'fucking']}
+app.use('/assets', express.static('assets'));
+
+app.get(['/', '/profile'], function(req, res){
+  var data = {name:"Darshan nagarajappa" , age:24,
+    hobbies:['eating', 'sleeping', 'fucking']};
   res.render('profile', {data:data});
+});
+
+app.post('/contact', urlencodedParser, function (req, res) {
+    console.log(req.body);
+    res.render('contact-sucess', {data: req.body})
+});
+
+app.get('/contact', function(req, res){
+  //console.log(req.query)
+  res.render('contact', {qs: req.query})
 });
 
 
